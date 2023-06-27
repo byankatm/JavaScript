9c02878d3c289edaa116e259852c95419564f899
Byanka Tomaz <125160119+byankatm@users.noreply.github.com>
2023-06-27 14:26:13 -0300

    Add files via upload

--- cursoJS/aula5/script.js
+++ cursoJS/aula5/script.js
@@ -1,3 +1,20 @@
-let num1 = 30.2341
+const numero = Number(prompt("Digite um número: "))
 
-console.log(num1.toFixed(2)) 
+const numeroTitulo = document.getElementById("numero-titulo")
+const numeroTitu = document.getElementById("numero")
+const raizQua = document.getElementById("raizQua")
+const isInte = document.getElementById("isInteiro")
+const isNaNN = document.getElementById("isNaN")
+const arreBaixo = document.getElementById("baixo")
+const arreCima = document.getElementById("cima")
+const decimais = document.getElementById("duasDecimais")
+
+
+numeroTitulo.innerHTML = numero
+numeroTitu.innerHTML = numero
+raizQua.innerHTML = Math.sqrt(numero)
+isInte.innerHTML = Number.isInteger(numero)
+isNaNN.innerHTML = Number.isNaN(numero)
+arreBaixo.innerHTML = Math.floor(numero)
+arreCima.innerHTML = Math.ceil(numero)
+decimais.innerHTML = numero.toFixed(2)
