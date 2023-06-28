1e2c60b7181f9db40d586bddacb5c98051113073
Byanka Tomaz <125160119+byankatm@users.noreply.github.com>
2023-06-28 17:24:59 +0000

    update

--- cursoJS/IMC/assets/js/index.js
+++ cursoJS/IMC/assets/js/index.js
@@ -1,10 +1,22 @@
 function escopoImc() {
 
-    const form = document.querySelector('.formulario');
-    const resultado = document.querySelector('.resultado');
+    const form = document.querySelector('#formulario');
+    const resultado = document.querySelector('#resultado');
 
     function calculoImc (event){
         event.preventDefault();
+
+        const peso = form.querySelector('#peso');
+        const altura = form.querySelector('#altura');
+
+        const soma = peso.value / (altura.value**2);
+
+        console.log(soma.toFixed(2))
+
     }
 
+    form.addEventListener('submit', calculoImc);
+
 }
+
+escopoImc()
