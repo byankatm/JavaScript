a9af8768e5394f4910c4a9e049985d1a5a81c652
Byanka Tomaz <125160119+byankatm@users.noreply.github.com>
2023-07-17 08:04:07 -0300

    Add files via upload

---
+++ cursoJS/aula14/index.js
@@ -0,0 +1,15 @@
+function mostraHota(){
+    let data = new Date();
+    
+    return data.toLocaleTimeString('pt-BR', {
+        hour12: false
+    });
+}
+
+const timer = setInterval(function() {
+    console.log(mostraHota());
+}, 1000);
+
+setTimeout(function(){
+    clearInterval(timer);
+}, 3000);
