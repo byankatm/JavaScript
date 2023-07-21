8aac68a15d02a6819e5bfd7d57a9c75e02a5f810
Byanka Tomaz <125160119+byankatm@users.noreply.github.com>
2023-07-21 13:08:26 -0300

    Delete JavaScript/cursoJS directory

--- JavaScript/cursoJS/aula8/index.js
+++
@@ -1,8 +0,0 @@
-/* 
-Dados primitivos js (imutaveis) = string, number, boolean, undefined, null (bigint, symbol) 
-
-valor passado por referencia(mutaveis) = array, object, function
-*/
-
-let a = [1, 2, 3]
-let b = [... a] // estamos copiando o valor a para dentro do b, o b esta sendo totalmente idependente
